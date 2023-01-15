19b30c0726426d3c344ddfb93275c7132f21e05a
RxGoodness <adedokuntoyyib@gmail.com>
2023-01-15 23:59:10 +0100

    configuration files and logic

---
+++ src/config/config.ts
@@ -0,0 +1,51 @@
+// import { createConnection } from 'typeorm';
+
+// export const secret = "yoursecretkey";
+
+// createConnection({
+//     type: 'mysql',
+//     host: 'localhost',
+//     port: 3306,
+//     username: 'your_username',
+//     password: 'your_password',
+//     database: 'your_database_name',
+//     entities: [User, Tweet, Like, Comment],
+//     synchronize: true,
+// }).then(connection => {
+//     console.log('Connection to the database established!');
+// }).catch(error => {
+//     console.log(error);
+// });
+
+import { createConnection } from 'typeorm';
+// import { DataSource } from 'typeorm';
+import {User, Tweet, Like} from '../entity'
+
+export const DATABASE_CONNECTION = createConnection
+// new DataSource
+({
+    type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '287693@Ata',
+      database: 'google_oauth2_app',
+    entities: [
+        // User, Tweet, Like
+        __dirname + "/entities/user.ts"
+    ],
+    synchronize: true,
+    "migrations": [
+        "src/migration/**/*.ts"
+      ],
+      "subscribers": [
+        "src/subscriber/**/*.ts"
+      ],
+    //   "cli": {
+    //     "entitiesDir": "src/entity",
+    //     "migrationsDir": "src/migration",
+    //     "subscribersDir": "src/subscriber"
+    //   }
+});
+
+export const SECRET = "yoursecretkey";

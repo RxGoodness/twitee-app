f5f432d16723702b214483d8bfa3bd34ab2ca104
RxGoodness <adedokuntoyyib@gmail.com>
2023-01-27 22:48:55 +0100

    file database modifications

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,63 @@
 
+// import { createConnection, DataSourceOptions } from 'typeorm';
+// import { DataSource } from 'typeorm';
+// import {User, Tweet, Like} from '../entity'
+
+// export const DATABASE_CONNECTION = 
+// createConnection
+// ({
+//     type: 'postgres',
+//       port: 5432,
+//         host: "dpg-cf4648mn6mpos6v3fn50-a",
+//         username: "rxgoodness",
+//         password: "OVNxBujUsoolINm34H5FuUN25T0JtQdW",
+//         database: "rxgoodness",
+//     entities: [
+//         // User, Tweet, Like
+//         __dirname + '/../**/*.entity.{js,ts}'
+//     ],
+//     synchronize: true,
+//     "migrations": [
+//         "src/migration/**/*.ts"
+//       ],
+//       "subscribers": [
+//         "src/subscriber/**/*.ts"
+//       ],
+// })
+// export const AppDataSource = new DataSource({
+//   type: 'mysql',
+//   host: 'localhost',
+//   port: 3306,
+//   username: 'root',
+//   password: '287693@Ata',
+//   database: 'google_oauth2_app',
+// entities: [
+//     // User, Tweet, Like
+//     __dirname + '/../**/*.entity.{js,ts}'
+// ],
+// synchronize: true,
+// "migrations": [
+//     "src/migration/**/*.ts"
+//   ],
+//   "subscribers": [
+//     "src/subscriber/**/*.ts"
+//   ],
+// //   "cli": {
+// //     "entitiesDir": "src/entity",
+// //     "migrationsDir": "src/migration",
+// //     "subscribersDir": "src/subscriber"
+// //   }
+// // });
+// })
+
+
+
+
+
+
+
+
+
 import { createConnection, DataSourceOptions } from 'typeorm';
 import { DataSource } from 'typeorm';
 import {User, Tweet, Like} from '../entity'
@@ -6,12 +65,12 @@ import {User, Tweet, Like} from '../entity'
 export const DATABASE_CONNECTION = 
 createConnection
 ({
-    type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '287693@Ata',
-      database: 'google_oauth2_app',
+  type: 'postgres',
+  port: 5432,
+    host: "localhost",
+    username: "rxgoodness",
+    password: "287693@Ata",
+    database: "rxgoodness",
     entities: [
         // User, Tweet, Like
         __dirname + '/../**/*.entity.{js,ts}'
@@ -25,12 +84,12 @@ createConnection
       ],
 })
 export const AppDataSource = new DataSource({
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '287693@Ata',
-  database: 'google_oauth2_app',
+  type: 'postgres',
+  port: 5432,
+    host: "localhost",
+    username: "rxgoodness",
+    password: "287693@Ata",
+    database: "rxgoodness",
 entities: [
     // User, Tweet, Like
     __dirname + '/../**/*.entity.{js,ts}'
